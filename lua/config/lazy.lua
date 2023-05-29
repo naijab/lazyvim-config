e757a44d492838ddb41ddf3a5973c5bb0e5ee7dd
Nattapon Pondongnok <nainatjab999@gmail.com>
2023-05-29 15:49:41 +0700

    add support golang and nightowl theme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,8 +11,22 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
+    { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.dap.nlua" },
+    { import = "lazyvim.plugins.extras.test.core" },
+
+    -- Golang
+    { import = "lazyvim.plugins.extras.lang.go" },
+
+    -- typescript
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+
+    -- css
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "oxfist/night-owl.nvim",
+  },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "night-owl",
+    },
+  },
+}
