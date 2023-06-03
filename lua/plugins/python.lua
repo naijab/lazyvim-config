96a3cdd369e62b9fc0421ae7fd6f0cdd9ae9e0bd
Nattapon Pondongnok <nainatjab999@gmail.com>
2023-06-03 17:19:29 +0700

    add support python lsp

---
+++ lua/plugins/python.lua
@@ -0,0 +1,39 @@
+return {
+  -- add python to treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "python" })
+      end
+    end,
+  },
+
+  -- correctly setup mason lsp extensions
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "python-lsp-server" })
+      end
+    end,
+  },
+
+  -- correctly setup mason dap extensions
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "python" })
+      end
+    end,
+  },
+
+  -- correctly setup lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("lspconfig").pylsp.setup({})
+    end,
+  },
+}
